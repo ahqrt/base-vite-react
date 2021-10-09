c8dd4f60fefc41cc84e5ce477512ef5a8a14e5a5
ahqrt <1217223113@qq.com>
2021-10-09 16:58:58 +0800

    添加git commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import './app.css'
 
 const App: FC = () => {
+    const [count, setCount] = useState(0)
+
+    const add = (count: number) => {
+        setCount(count)
+    }
     useEffect(() => {
         console.log(123)
     }, [])
+
     return (
         <div>
             <h2>Welcome to vite-react-cli</h2>
